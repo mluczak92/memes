69cb618846eaaf3ef27bfb76a678d0aa2c126697
Marcin <mluczak92@gmail.com>
2020-09-20 21:42:50 +0200

    styles, .gitignore, default.jpg

--- memes/Models/InMemoryPostsRepository.cs
+++ memes/Models/InMemoryPostsRepository.cs
@@ -4,10 +4,10 @@ using System.Linq;
 namespace memes.Models {
     public class InMemoryPostsRepository : IPostsRepository {
         List<Post> posts = new List<Post>() {
-            new Post() { Title = "Raz", Description = "Opis pierwszy", ImageName = "1asn0ykh.nfy.jpg" },
-            new Post() { Title = "Dwa Dwa Dwa Dwa", ImageName = "1asn0ykh.nfy.jpg" },
-            new Post() { Title = "Trzy", Description = "Opis trzeci trzeci, trzeci, trzecitrzecitrzecitrzeci trzeci trzeci", ImageName = "1asn0ykh.nfy.jpg" },
-            new Post() { Title = "Cztery Cztery", Description = "Opis czwarty", ImageName = "1asn0ykh.nfy.jpg" },
+            new Post() { Title = "Raz", Description = "Opis pierwszy", ImageName = "default.jpg" },
+            new Post() { Title = "Dwa Dwa Dwa Dwa", ImageName = "default.jpg" },
+            new Post() { Title = "Trzy", Description = "Opis trzeci trzeci, trzeci, trzecitrzecitrzecitrzeci trzeci trzeci", ImageName = "default.jpg" },
+            new Post() { Title = "Cztery Cztery", Description = "Opis czwarty", ImageName = "default.jpg" },
         };
 
         public IQueryable<Post> Posts => posts.AsQueryable();
